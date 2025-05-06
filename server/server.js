5d8976824a430c7fba4e5560c0895947854bae15
Tushar-459 <tusharkhairnar246@gmail.com>
2025-05-06 17:33:59 +0530

    update service

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", methods: ["GET", "POST"], credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://register-ui-8vrh.onrender.com' /*"http://localhost:3000"*/, methods: ["GET", "POST"], credentials: true }));
 app.use(express.json());
 
 // Routes
